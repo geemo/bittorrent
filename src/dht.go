9b314939b520d23027b65ced069150aa94cb68cf
王金海 <jinhai.wang@ele.me>
2017-06-22 17:58:45 +0800

    add krpc

--- src/dht.go
+++ src/dht.go
@@ -1,1 +1,29 @@
 package dht
+
+import "net"
+
+// DHT struct
+type DHT struct {
+	selfNode      *Node
+	selfAddr      *net.UDPAddr
+	rootNode      *BitMap
+	k             *Krpc
+	transactionID int
+}
+
+// ips, err := net.LookupIP("router.bittorrent.com")
+var maxTansactionID = 0xffffffff
+
+// NewDHT struct
+func NewDHT() *DHT {
+	return &DHT{}
+}
+
+func (dht *DHT) getTransactionID() int {
+	if dht.transactionID >= maxTansactionID {
+		dht.transactionID = 0
+		return dht.transactionID
+	}
+	dht.transactionID++
+	return dht.transactionID
+}
--- src/krpc.go
+++ src/krpc.go
@@ -1,8 +1,9 @@
 package dht
 
 import (
-	"fmt"
+	"io/ioutil"
 	"net"
+	"strconv"
 )
 
 // Krpc struct
@@ -13,13 +14,7 @@ type Krpc struct {
 // NewKrpc instance
 func NewKrpc(ip string, port int) (Krpc, error) {
 	var k Krpc
-	ips, err := net.LookupIP("router.bittorrent.com")
-	if err != nil {
-		return k, err
-	}
-	fmt.Println(ips, "00")
 	srcAddr := &net.UDPAddr{IP: net.IPv4zero, Port: 0}
-	// dstAddr := &net.UDPAddr{IP: ips[0], Port: 6881}
 	conn, err := net.ListenUDP("udp", srcAddr)
 	if err != nil {
 		return k, err
@@ -34,23 +29,68 @@ func (k *Krpc) send(data Map, addr *net.UDPAddr) (Map, error) {
 		return nil, err
 	}
 	k.conn.WriteToUDP(b, addr)
-	res := make([]byte, 1024)
-	n, err := k.conn.Read(res)
+	res, err := ioutil.ReadAll(k.conn)
 	if err != nil {
 		return nil, err
 	}
-	msg, err := Decode(res[:n])
 
+	msg, err := Decode(res)
 	if err != nil {
 		return nil, err
 	}
 	return msg.(Map), nil
 }
 
-func ping() {}
+func (k *Krpc) ping(dht *DHT, addr *net.UDPAddr) (Map, error) {
+	query := Map{
+		"t": strconv.Itoa(dht.getTransactionID()),
+		"y": "q",
+		"q": "ping",
+		"a": Map{
+			"id": dht.selfNode.id.RawString(),
+		},
+	}
+	return k.send(query, addr)
+}
 
-func findNode() {}
+func (k *Krpc) findNode(dht *DHT, addr *net.UDPAddr, targetNode *Node) (Map, error) {
+	query := Map{
+		"t": strconv.Itoa(dht.getTransactionID()),
+		"y": "q",
+		"q": "find_node",
+		"a": Map{
+			"id":     dht.selfNode.id.RawString(),
+			"target": targetNode.id.RawString(),
+		},
+	}
+	return k.send(query, addr)
+}
 
-func (k *Krpc) getPeers() {}
+func (k *Krpc) getPeers(dht *DHT, addr *net.UDPAddr, infoHash *BitMap) (Map, error) {
+	query := Map{
+		"t": strconv.Itoa(dht.getTransactionID()),
+		"y": "q",
+		"q": "get_peers",
+		"a": Map{
+			"id":        dht.selfNode.id.RawString(),
+			"info_hash": infoHash.RawString(),
+		},
+	}
+	return k.send(query, addr)
+}
 
-func (k *Krpc) announcePeer() {}
+func (k *Krpc) announcePeer(dht *DHT, addr *net.UDPAddr, token string, infoHash *BitMap) (Map, error) {
+	query := Map{
+		"t": strconv.Itoa(dht.getTransactionID()),
+		"y": "q",
+		"q": "announce_peer",
+		"a": Map{
+			"implied_port": 1,
+			"port":         12231,
+			"id":           dht.selfNode.id.RawString(),
+			"info_hash":    infoHash.RawString(),
+			"token":        token,
+		},
+	}
+	return k.send(query, addr)
+}
